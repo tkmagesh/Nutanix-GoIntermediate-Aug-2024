050f1de16d2b81f961270334cef93ff9c71dbd48
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-26 10:11:34 +0530

    interfaces recap

---
+++ 01-Recap/01-interfaces.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+// Ver 1.0
+type MessageGenerator struct {
+}
+
+func (mg *MessageGenerator) GetMessage() string {
+	return "Have a nice day!"
+}
+
+func NewMessageGenerator() *MessageGenerator {
+	return &MessageGenerator{}
+}
+
+/* Contract */
+
+type IMessageGenerator interface {
+	GetMessage() string
+}
+
+type Greeter struct {
+	messageGenerator IMessageGenerator
+}
+
+func (g *Greeter) Greet(userName string) string {
+	return fmt.Sprintf("Hi %s, %s", userName, g.messageGenerator.GetMessage())
+}
+
+func NewGreeter(mg IMessageGenerator) *Greeter {
+	greeter := &Greeter{
+		messageGenerator: mg,
+	}
+	return greeter
+}
+
+// ver 2.0
+type GoodDayMessageGenerator struct {
+}
+
+func (gdmg *GoodDayMessageGenerator) GetMessage() string {
+	return "Have a good day!"
+}
+
+func NewGoodDayMessageGenerator() *GoodDayMessageGenerator {
+	return &GoodDayMessageGenerator{}
+}
+
+func main() {
+	// tight coupling
+	/*
+		greeter := &Greeter{}
+		fmt.Println(greeter.Greet("Magesh"))
+	*/
+
+	// loose coupling using contracts
+	/*
+		mg := NewMessageGenerator()
+		greeter := NewGreeter(mg)
+	*/
+
+	// ver 2.0
+	// use a different implementation for generating greet messages (GoodDayMessageGenerator)
+	gdmg := NewGoodDayMessageGenerator()
+	greeter := NewGreeter(gdmg)
+	fmt.Println(greeter.Greet("Magesh"))
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Go Intermediate
+
+## Magesh Kuppan
+- tkmagesh77@gmail.com
+
+## Software Requirements
+- go tools
+- VS Code (any editor)
+
+## Pre-requisites
+- Must be familiar with the following
+    - Variables, Constants & Data Types
+    - Control Structures
+    - Collection Types
+    - Error Handling
+    - Higher Order Functions
+    - Deferred Functions
+    - Panic & Recovery
+    - Structs & Methods
+    - Interfaces
+
+## Schedule
+| What | When |
+|------|------|
+| Commence | 9:30 AM |
+| Tea Break | 11:00 AM (20 mins) |
+| Lunch Break | 01:00 PM (1 hour) |
+| Tea Break | 03:15 PM (20 mins) |
+| Wind up | 4:30 PM |
+## Agenda
+### Day-1
+- Concurrency
+- Concurrency Patterns
+- Testing
+
+### Day-2
+- IO
+- Database Programming
+- HTTP Services
+
+### References
+- Building Object Oriented Software Guided By Tests
+
+## Recap
+### Interfaces
+- Contracts
+- Loose Coupling
+- Implicitly Implemented
+
+### Panic & Recovery
+### Higher Order Functions
