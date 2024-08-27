3ef28c3e9a0fc84c81235da1ef650c74064073be
Magesh Kuppan <tkmagesh77@gmail.com>
2024-08-27 10:17:05 +0530

    signaling using channels & select-case

---
+++ 02-concurrency/07-multi-channel/21-example.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := genNos()
+	for no := range ch {
+		fmt.Println(no)
+	}
+}
+
+func genNos() <-chan int {
+	ch := make(chan int)
+	timeoutCh := timeout(5 * time.Second)
+	go func() {
+	LOOP:
+		for i := 1; ; i++ {
+			select {
+			case ch <- i * 10:
+				time.Sleep(500 * time.Millisecond)
+			case <-timeoutCh:
+				fmt.Println("timeout occurred!")
+				break LOOP
+			}
+		}
+		close(ch)
+	}()
+	return ch
+}
+
+func timeout(d time.Duration) <-chan time.Time {
+	timeOutCh := make(chan time.Time)
+	go func() {
+		time.Sleep(d)
+		timeOutCh <- time.Now()
+	}()
+	return timeOutCh
+}
